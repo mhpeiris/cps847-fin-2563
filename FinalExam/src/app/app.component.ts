bda656750542f5901612870889111601630d39ef
Hareen Peiris <hareenmpeiris@gmail.com>
2017-04-22 11:18:26 -0400

    angular2_app

---
+++ FinalExam/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <ul>
+      <li><a routerLink="/">Home</a></li>
+      <li><a routerLink="/distance">distance</a></li>
+    </ul>
+    <router-outlet></router-outlet>
+  `,
+})
+export class AppComponent  {}
+
